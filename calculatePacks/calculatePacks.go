5e3a2dab3501aa72a0301ce220035d91e9aca94c
Tom Sheehy <tomsheehy12@yahoo.co.uk>
2023-12-18 22:31:26 +0000

    Final working code

--- calculatePacks/calculatePacks.go
+++ calculatePacks/calculatePacks.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+	PacksRequired []int `json:"packsRequired"`
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Lambda will call the Handler function
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	itemsOrderedStr, ok := request.QueryStringParameters["itemsOrdered"]
+
+	if !ok {
+		log.Println("Missing 'itemsOrdered' query parameter")
+		return events.APIGatewayProxyResponse{}, fmt.Errorf("Missing 'itemsOrdered' query parameter")
+	}
+
+	itemsOrdered, _ := strconv.Atoi(itemsOrderedStr)
+
+	packs, err := getPackSizes()
+
+	if err != nil {
+		log.Print("Error encountered while trying to get pack sizes:", err)
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	sortedPackSizes := sortPackSizes(packs.PackSizes)
+	possiblePackSizes := possiblePackCombinations(sortedPackSizes)
+	combinations := findCombinations(sortedPackSizes, possiblePackSizes)
+
+	packsRequired := handlePacks(itemsOrdered, sortedPackSizes, possiblePackSizes, combinations)
+
+	responseData := Response{
+		PacksRequired: packsRequired,
+	}
+
+	responseJSON, err := json.Marshal(responseData)
+	if err != nil {
+		log.Print("Error marshaling JSON:", err)
+		return events.APIGatewayProxyResponse{}, fmt.Errorf("Error creating JSON response")
+	}
+
+	headers := map[string]string{
+		"Access-Control-Allow-Origin":  "localhost:3000, https://pack-optimisation-d42ubt7qr-toms-projects-41b46903.vercel.app/, *",
+		"Access-Control-Allow-Headers": "Content-Type",
+		"Access-Control-Allow-Methods": "OPTIONS, GET, POST, ANY",
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(responseJSON),
+		StatusCode: 200,
+		Headers:    headers,
+	}, nil
 }
--- calculatePacks/go.mod
+++ calculatePacks/go.mod
@@ -1,3 +1,8 @@
 module calculatePacks
 
 go 1.21.5
+
+require (
+	github.com/aws/aws-lambda-go v1.42.0 // indirect
+	github.com/aws/aws-sdk-go v1.49.4 // indirect
+)
